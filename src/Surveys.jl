5ecb94aaab2a2615a04cadcd4d3e9ee5412deccd
Sam Anklesaria <anklesarian@gmail.com>
2026-01-24 15:02:25 -0600

    Stratification

--- src/Surveys.jl
+++ src/Surveys.jl
@@ -1,65 +1,73 @@
 module Surveys
-using Statistics, StatsBase
+using Statistics, StatsBase, DataFrames
 
-struct FiniteSample{W<:AbstractWeights,T<:Real}
-    N::T
-    weights::W
+# General Utilities
+# TODO: perhaps these are unnecessary. Easier just to use DataFramesMeta manually.
+
+function stratified(f, df::DataFrame, cols, Ns::Dict, strata::Symbol)
+    sum(combine(groupby(df, strata), ([strata; cols] => ((s, c...)->f(c..., Ns[s])) => :val)[!, :val]))
 end
 
-struct SRSNoReplace <: AbstractWeights{Int,Int,Vector{Int}}
+unstratified(f, df::DataFrame, cols, N) = f(df[!, cols]..., N)
+
+# SI Designs
+
+Base.sum(xs::Vector, N::Int) = N * mean(xs)
+
+function sum_var(xs::Vector, N::Int)
+    N^2 * (1 / length(x) - 1 / N) * var(xs; corrected=true)
 end
 
-struct NoReplaceProbabilityWeights{T<:Real} <: AbstractWeights{T,T,Vector{T}}
-    probs::Vector{T}
-    joint_probs::Matrix{T}
-end
+# SIR Designs
 
-FiniteSample(N::Int) = FiniteSample(N, SRSNoReplace())
+Base.sum(xs::Vector, probs::Vector, N::Int) = sum(xs ./ probs) # also works for non-replacement
+# Note: same as in StatsBase.
 
-# Sampling with replacement
-# TODO: if we have N, we should rake the weights.
-
-Base.sum(x::AbstractVector, w::FiniteSample{<:ProbabilityWeights}) = sum(x, w.weights)
-function Statistics.var(::typeof(sum), x::AbstractVector, w::FiniteSample{<:ProbabilityWeights})
-    var(x, w.weights; corrected=true) / length(x)
-end
-
-function Statistics.mean(::typeof(sum), x::AbstractVector, w::FiniteSample{<:ProbabilityWeights})
-    var(sum, x, w) / w.N
-end
-
-# Simple random sampling without replacement
-
-Base.sum(x::AbstractVector, w::FiniteSample{Nothing}) = w.N * mean(x)
-
-function Statistics.var(::typeof(mean), x::AbstractVector, w::FiniteSample{Nothing})
-    (1 / length(x) - 1 / w.N) * var(x; corrected=true)
-end
-
-function Statistics.var(::typeof(sum), x::AbstractVector, w::FiniteSample{Nothing})
-    w.N^2 * mean(sum, x, w)
+function sum_var(xs::Vector, probs::Vector, N::Int)
+    N^2 * var(xs, ProbabilityWeights(1./ probs); corrected=true) / length(xs)
 end
 
 # General sampling without replacement
 
-Base.sum(x::AbstractVector, w::FiniteSample{<:NoReplaceProbabilityWeights}) = sum(x, ProbabilityWeights(w.weights.probs))
-
-function Statistics.var(::typeof(sum), x::AbstractVector, w::FiniteSample{NoReplaceProbabilityWeights})
-    Δ = (1 .- (w.weights.probs .* w.weights.probs')) ./ w.weights.joint_probs
-    Δ[diagind(Δ)] .= 1 .- w.weights.probs
-    y = x ./ w.weights.probs
+sum_var(strata_info::Tuple{Int, Matrix}) = (x, probs)-> begin
+    (joint_probs, N) = strata_info
+    Δ = (1 .- (probs .* probs')) ./ joint_probs
+    Δ[diagind(Δ)] .= 1 .- probs
+    y = x ./ probs
     y' * (Δ * y)
 end
 
-# Next up: stratified designs.
-# We need the size of each strata, and what strata each observation belongs to.
-# This becomes easier with DataFrames
-# TODO
+# Two Stage Cluster Sampling
+
+# @chain df begin
+#   @groupby(cluster)
+# 	@combine(:total= sum(:xs, N2s[:cluster[1]]))
+# 	@select(result=sum(:total, N1))
+# end
+
+# One Stage Cluster Sampling
+# @chain df begin
+#     @groupby(cluster)
+# 	@combine(:total= sum(:xs))
+# 	@select(result=sum(:total, N1))
+# end
+
+# This same pattern (above) can be used to get overall variance, just using `sum_var` instead of sum.
+
+# What about CIs? Maybe have a type that captures both variance and point estimate? Could use Normal from Distributions.
+# What about sampling clusters?
+# When we're sampling whole groups, at least for the total, just sum the clusters first, and use the existing functions above.
+
+
+
+
+# When two stage sampling with replacement, we can just multiply the probabilities.
+# to get a pwr estimate.
+# NOT quite- that doesn't account for a lack of independence.
+
+# TODO: other functions.
 
 # Sampling Designs to Support:
-# Independent sampling without replacement but unequal probabilities and known N
-# Independent sampling without replacement but unequal probabilities without known N
-# Stratified (of another design)
 # Cluster sampling (clusters of one form, within of another)
 # Given replicate weights.
 # Linearization
